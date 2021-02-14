294d0d8a8185f20fca6881b47fce7f3daf290827
Vladilen Karassev <Vladilen@Karassev.com>
2021-02-14 15:17:49 -0600

    Added get all tenant pages

--- list/Add-ListItems_bulk.ps1
+++ list/Add-ListItems_bulk.ps1
@@ -1,16 +1,37 @@
-﻿$siteUrl = "https://uhgdev.sharepoint.com/sites/TestTeamSite_01"
-Connect-PnPOnline -Url $siteUrl -ClientId $clientID -Certificate $cert -Tenant $tenant -CertificatePassword $certPw -Verbose
+﻿$siteUrl = "https://$orgname.sharepoint.com/sites/TestTeamSite_01"
+Connect-PnPOnline -Url $siteUrl -ClientId $clientID -CertificateBase64Encoded $secretPlainText -Tenant $tenant 
 Get-PnPSite | ft -a
 
-$list = Get-PnPList -Identity "LargeList"
+$list = Get-PnPList -Identity "LargeList2"
 $list | ft -a
 $list.ItemCount
 
 # add list items
-for ($i = 0; $i -lt 5500; $i++) {
-    if ($i % 10) { Write-Host $i " " -NoNewline }
-    $addedItem = Add-PnPListItem -List $list -Values @{"Title" = "Test Title " + $( -join ((65..90) + (97..122) | Get-Random -Count 15 | % { [char]$_ })); "Number" = $(Get-Random -Minimum 1 -Maximum 10000 ) }
+# withOut BATCHES
+$timeStart = Get-Date
+for ($i = 0; $i -lt 500; $i++) {
+    if ($i % 10) {} else { Write-Host $i " " -NoNewline }
+    $title = "Test Title " + $( -join ((65..90) + (97..122) | Get-Random -Count 15 | % { [char]$_ }))
+    $number = $(Get-Random -Minimum 12000 -Maximum 13000 )
+    $values = @{"Title" = $title; "Number" = $number }
+    $addedItem = Add-PnPListItem -List $list -Values $values
 }
+$timeFinish = Get-Date
+$timeElapsed = $timeFinish - $timeStart
+$timeElapsed.TotalSeconds
+
+# with BATCHES
+$timeStart = Get-Date
+$batch = New-PnPBatch
+1..1000 | ForEach-Object { 
+    $title = "Test Title $_" + $( -join ((65..90) + (97..122) | Get-Random -Count 15 | ForEach-Object { [char]$_ }))
+    $values = @{"Title" = $title }
+    Add-PnPListItem -List $list -Values $values -Batch $batch 
+}
+Invoke-PnPBatch -Batch $batch
+$timeFinish = Get-Date
+$timeElapsed = $timeFinish - $timeStart
+$timeElapsed.TotalSeconds
 
 #Get-PnPListItem -List Tasks -PageSize 1000 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } | % { $_.BreakRoleInheritance($true, $true) }
 Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } 
@@ -18,7 +39,7 @@ Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items | 
 Get-PnPListItem -List $list -Fields "ID" -PageSize 100 
 Get-PnPListItem -List $list -Fields "ID" -PageSize 100 -ScriptBlock { Param($items) $items | Sort-Object -Descending | ForEach-Object { $_.DeleteObject() } } 
 
-Remove-PnPList -Identity $list
+#Remove-PnPList -Identity $list
 
 
 
--- list/Remove-SPOListItems.ps1
+++ list/Remove-SPOListItems.ps1
@@ -1,26 +1,61 @@
-﻿$siteUrl = "https://kar1.sharepoint.com/sites/LargeLists"
+﻿$siteUrl = "https://$orgname.sharepoint.com/sites/TestTeamSite_01"
 #New-SPOSite -Url $siteUrl -Owner $adminUPN -Template "sts#3" -StorageQuota 2048
 
 Connect-PnPOnline -Url $siteUrl -Credentials $cred
 Get-PnPSite | ft -a
 
-$list = Get-PnPList -Identity "LargeList"
+$list = Get-PnPList -Identity "LargeList2"
 $list | ft -a
 $list.ItemCount
 
-# add list items
-for ($i = 0; $i -lt 5500; $i++) {
-    if ($i % 10) { Write-Host $i " " -NoNewline }
-    $addedItem = Add-PnPListItem -List $list -Values @{"Title" = "Test Title " + $( -join ((65..90) + (97..122) | Get-Random -Count 15 | % { [char]$_ })); "Number" = $(Get-Random -Minimum 1 -Maximum 10000 ) }
-}
+# remove Items 
+# plain
+$timeStart = Get-Date
+9001..10000 | Foreach-Object { Remove-PnPListItem -List $list -Identity $_ -Force:$true }
+$timeFinish = Get-Date
+$timeElapsed = $timeFinish - $timeStart
+$timeElapsed.TotalSeconds
 
+
+
+
+# with script block
 #Get-PnPListItem -List Tasks -PageSize 1000 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } | % { $_.BreakRoleInheritance($true, $true) }
 Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } 
 Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items | ForEach-Object { $_.DeleteObject() } } 
 Get-PnPListItem -List $list -Fields "ID" -PageSize 100 
-Get-PnPListItem -List $list -Fields "ID" -PageSize 100 -ScriptBlock { Param($items) $items | Sort-Object -Descending | ForEach-Object { $_.DeleteObject() } } 
 
-Remove-PnPList -Identity $list
+$timeStart = Get-Date
+Get-PnPListItem -List $list -Fields "ID" -PageSize 100 -ScriptBlock { Param($items) $items | Sort-Object -Descending | ForEach-Object { $_.DeleteObject() } } 
+$timeFinish = Get-Date
+$timeElapsed = $timeFinish - $timeStart
+$timeElapsed.TotalSeconds
+
+# remove Items with batches
+$timeStart = Get-Date
+$batch = New-PnPBatch
+2001..9000 | Foreach-Object { Remove-PnPListItem -List $list -Identity $_ -Batch $batch }
+Invoke-PnPBatch -Batch $batch
+$timeFinish = Get-Date
+$timeElapsed = $timeFinish - $timeStart
+$timeElapsed.TotalSeconds
+
+$list = Get-PnPList -Identity "LargeList2"
+$list.ItemCount
+
+$items = Get-PnPListItem -List $list -PageSize 500
+$items.count
+$items[0].Id
+$items[-1].Id
+
+
+
+
+
+
+
+
+#Remove-PnPList -Identity $list
 
 
 
