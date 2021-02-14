c15984f29dbd0958978484b61e33667fb1c4c1eb
Vladilen Karassev <Vladilen@Karassev.com>
2021-02-14 09:48:07 -0600

    Added get all tenant pages

---
+++ list/Add-ListItems_bulk.ps1
@@ -0,0 +1,25 @@
+﻿$siteUrl = "https://uhgdev.sharepoint.com/sites/TestTeamSite_01"
+Connect-PnPOnline -Url $siteUrl -ClientId $clientID -Certificate $cert -Tenant $tenant -CertificatePassword $certPw -Verbose
+Get-PnPSite | ft -a
+
+$list = Get-PnPList -Identity "LargeList"
+$list | ft -a
+$list.ItemCount
+
+# add list items
+for ($i = 0; $i -lt 5500; $i++) {
+    if ($i % 10) { Write-Host $i " " -NoNewline }
+    $addedItem = Add-PnPListItem -List $list -Values @{"Title" = "Test Title " + $( -join ((65..90) + (97..122) | Get-Random -Count 15 | % { [char]$_ })); "Number" = $(Get-Random -Minimum 1 -Maximum 10000 ) }
+}
+
+#Get-PnPListItem -List Tasks -PageSize 1000 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } | % { $_.BreakRoleInheritance($true, $true) }
+Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items.Context.ExecuteQuery() } 
+Get-PnPListItem -List $list -PageSize 100 -ScriptBlock { Param($items) $items | ForEach-Object { $_.DeleteObject() } } 
+Get-PnPListItem -List $list -Fields "ID" -PageSize 100 
+Get-PnPListItem -List $list -Fields "ID" -PageSize 100 -ScriptBlock { Param($items) $items | Sort-Object -Descending | ForEach-Object { $_.DeleteObject() } } 
+
+Remove-PnPList -Identity $list
+
+
+
+# | ForEach-Object { Write-Host $_.Title }
