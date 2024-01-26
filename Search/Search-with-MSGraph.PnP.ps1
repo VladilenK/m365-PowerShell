2ace314d861ca6948f0c8db4550d7114944299cd
Vladilen Karassev <Vladilen@Karassev.com>
2024-01-26 10:47:17 -0600

    upd

--- Search/Search-with-MSGraph.PnP.ps1
+++ Search/Search-with-MSGraph.PnP.ps1
@@ -1,21 +1,17 @@
-# MSAL
-# JVKdev app with delegated permissions
-$connectionDetails = @{
-    'TenantId'    = '046d11d6-08e0-4d20-a743-562836cdcab5'
-    'ClientId'    = 'cb3d2f17-f734-4512-b39d-25e0b7472205'
-    'Interactive' = $true
-}
+$conn = Connect-PnPOnline -Url $siteUrl -ClientId $clientid -Tenant $TenantId -Interactive
+$token = Get-PnPGraphAccessToken -Connection $conn
 
-$token = Get-MsalToken @connectionDetails
 
 $Headers = @{
-    'Authorization' = "bearer $($token.AccessToken)"
+    'Authorization' = "bearer $($token)"
 }
 Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/me' -Headers $Headers
 
 # search
 $query = "*"
-$query = "LastModifiedTimeForRetention<2021-01-01"
+$query = "*.pptx LastModifiedTimeForRetention<2023-09-01 site:https://s5dz3.sharepoint.com/sites/Mark8ProjectTeam"
+$query = "LastModifiedTimeForRetention<2023-09-01"
+
 $apiUrl = "https://graph.microsoft.com/beta/search/query"
 $body = @"
 { 
@@ -34,7 +30,8 @@ $body = @"
 
 $res = Invoke-RestMethod -Headers $Headers -Uri $apiUrl -Body $Body -Method Post -ContentType 'application/json'
 $res.value[0].searchTerms
-$res.value[0].hitsContainers[0].hits
+# $res.value[0].hitsContainers[0].hits
+$res.value[0].hitsContainers[0].hits.resource.WebUrl
 $res.value[0].hitsContainers[0].hits[0] | fl
 $res.value[0].hitsContainers[0].hits[1] | fl
 
