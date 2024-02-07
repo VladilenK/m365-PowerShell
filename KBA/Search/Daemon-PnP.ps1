6843f3a0393797e773ba297366536e0d1b2f6322
Vladilen Karassev <Vladilen@Karassev.com>
2024-02-07 15:43:58 -0600

    validated

--- KBA/Search/Daemon-PnP.ps1
+++ KBA/Search/Daemon-PnP.ps1
@@ -2,9 +2,9 @@
 Get-Module PnP.PowerShell -ListAvailable
 
 # Authentication
-$tenantId = '7ddc7314-9f01-45d5-b012-71665bb1c544'
-$clientId = 'aceed4f0-1fc0-487d-90d1-6ed9cafb2541'
-$certThumbprint = '160AE9107C82821A326363D4BCA82C1707C226B3'
+$tenantId = ''
+$clientId = ''
+$certThumbprint = ''
 
 $siteUrl = "https://s5dz3.sharepoint.com"
 Connect-PnPOnline -ClientId $clientid -Tenant $TenantId -Url $siteUrl -Thumbprint $certThumbprint
