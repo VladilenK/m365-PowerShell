2436c9db79d21e2904262c1317c5e07011bd229d
Vlad Karassev <V@mn7.xyz>
2025-03-20 11:55:46 -0500

    Search with Yammer API

--- Yammer/Yammer-Classic-v1-sample.ps1
+++ Yammer/Yammer-Classic-v1-sample.ps1
@@ -12,7 +12,22 @@ $results.messages | ForEach-Object {
 
 Invoke-WebRequest –Uri "https://www.yammer.com/api/v1/users.json" –Method Get -Headers $headers | ConvertFrom-Json | select email
 
+# search:
+$YammerApiURL = "https://www.yammer.com/api/v1/search.json?search=Test*"
+$results = Invoke-WebRequest –Uri $YammerApiURL –Method Get -Headers $headers | ConvertFrom-Json 
+$results
+$results.count
+$results.users[0]
+$results.messages.messages[0].body.parsed
+
+
+
+
+
 # admin only:
 Invoke-WebRequest –Uri "https://export.yammer.com/api/v1/export?since=2020-02-09T00:00:00+00:00" –Method Get -Headers $headers 
+
+
+
 
 
